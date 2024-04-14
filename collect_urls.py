832ee59cd65671ba76f1ca4e2bd4c559f6221ef2
ItsNiceCraft <74358857+ItsNiceCraft@users.noreply.github.com>
2024-04-14 16:30:09 +0200

    feat: indent output all_urls.json

--- collect_urls.py
+++ collect_urls.py
@@ -20,7 +20,7 @@ def main():
         print(f"Processed {num}/{len(version_manifest['versions'])}")
 
     with open("data/all_urls.json", "w") as f:
-        json.dump(downloads_db, f)
+        json.dump(downloads_db, f, indent=4)
 
 
 if __name__ == "__main__":
